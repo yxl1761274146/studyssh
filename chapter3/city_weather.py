f1540662dccce7af3c39c5f10680200bfd828387
yxl1761274146 <1761274146@qq.com>
2020-04-24 14:42:09 +0800

    41-yxl finish youdao

--- chapter3/city_weather.py
+++ chapter3/city_weather.py
@@ -1,6 +1,16 @@
 import requests
 
-url="https://cdn.heweather.com/china-city-list.txt"
-html=requests.get(url)
-html.encoding='utf8'
-print(html.text)
+class HeFeng():
+
+    def __init__(self):
+        self.url="https://cdn.heweather.com/china-city-list.txt"
+
+    def get_citys(self):
+        html = requests.get(self.url)
+        html.encoding='utf8'
+        print(html.text)
+        cities
+
+if __name__=='__main__':
+    hefeng=HeFeng()
+    hefeng.get_citys()
